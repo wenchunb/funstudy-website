dc923a57e8fb6d02782ed85b0c6fdcf2309b4180
Mr.wen <1285149319@qq.com>
2022-01-20 18:54:27 +0800

    真正完成初始化路由

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import {
   useRoutes,
+  Navigate
 } from "react-router-dom";
 
 import Login from '@/pages/login';
 // 配置路由v6使用useRoutes
 export const InitRouter = () => {
     const routers = useRoutes([
-        {path:'/',element:<Login/>}
+        {
+            path:'/',
+            exact:true,
+            element:<Login/>
+        },
+        {
+            path:'*',
+            // Navigate重定向
+            element:<Navigate to='/' />
+        }
     ])
     return routers
 }
